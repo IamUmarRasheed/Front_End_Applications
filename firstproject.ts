c2aac1e59d08715e5960ef5a23cc8735b303e9d3
umar <theumarrasheed@gmail.com>
2022-11-30 10:08:05 -0800

    typescript project00

---
+++ firstproject.ts
@@ -0,0 +1,137 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+
+const sleep = () => {
+  return new Promise((res) => {
+    setTimeout(res, 2000);
+  });
+};
+
+async function welcome() {
+  let animate = chalkAnimation.rainbow("Hello guys wellcome to my calculator");
+  await sleep();
+  animate.stop();
+  console.log(
+    chalk.yellow(`
+     _____________________
+    |  _________________  |
+    | | JO           0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|
+    `)
+  );
+}
+await welcome();
+function validateuser(input: any): boolean | string {
+  if (isNaN(input)) {
+    return "please eneter a valid number";
+  } else {
+    return true;
+  }
+}
+type take = {
+  Firstnumber: string;
+  Secondnumber: string;
+  operator: "+" | "-" | "*" | "/" | "%";
+};
+async function getuserinput() {
+  const userinput: take = await inquirer.prompt([
+    {
+      type: "input",
+      name: "Firstnumber",
+      message: "Enter your first number ",
+      validate: validateuser,
+    },
+    {
+      type: "list",
+      choices: ["+", "-", "/", "*", "%"],
+      name: "operator",
+      message: "Enter your operater",
+    },
+    {
+      type: "input",
+      name: "Secondnumber",
+      message: "Enter your Second number ",
+      validate: validateuser,
+    },
+  ]);
+  const Firstnumber = Number(userinput.Firstnumber);
+  const Secondnumber = Number(userinput.Secondnumber);
+  switch (userinput.operator) {
+    case "+":
+      console.log(
+        chalk.red(
+          `${Firstnumber}+${Secondnumber}=${(
+            Firstnumber + Secondnumber
+          ).toFixed(2)}`
+        )
+      );
+      break;
+    case "-":
+      console.log(
+        chalk.red(
+          `${Firstnumber}-${Secondnumber}=${(
+            Firstnumber - Secondnumber
+          ).toFixed(2)}`
+        )
+      );
+      break;
+    case "/":
+      console.log(
+        chalk.red(
+          `${Firstnumber}/${Secondnumber}=${(
+            Firstnumber / Secondnumber
+          ).toFixed(2)}`
+        )
+      );
+      break;
+    case "*":
+      console.log(
+        chalk.red(
+          `${Firstnumber}*${Secondnumber}=${(
+            Firstnumber * Secondnumber
+          ).toFixed(2)}`
+        )
+      );
+      break;
+    case "%":
+      console.log(
+        chalk.red(
+          `${Firstnumber}%${Secondnumber}=${(
+            Firstnumber % Secondnumber
+          ).toFixed(2)}`
+        )
+      );
+      break;
+    default:
+      break;
+  }
+}
+let confirm = async () => {
+  do {
+    await getuserinput();
+    var done = await inquirer.prompt({
+      type: "input",
+      name: "restart",
+      message: "Do You want to continue? press y/n",
+    });
+  } while (
+    done.restart === "y" ||
+    done.restart === "Y" ||
+    done.restart === "yes" ||
+    done.restart === "Yes"
+  );
+};
+
+confirm();
