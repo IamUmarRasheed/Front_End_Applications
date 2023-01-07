61f1afbe5092c17f620cd99b0ea121405aa0b3c1
umar <theumarrasheed@gmail.com>
2023-01-07 02:06:01 -0800

    wordcount

---
+++ word_count/Wordcount.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const user: { sentence: string } = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "ENTER YOUR SENTENCE TO COUNT",
+  },
+]);
+const userchoice = user.sentence.trim().split(" ");
+console.log(`Your sentence has ${userchoice.length} word`);
